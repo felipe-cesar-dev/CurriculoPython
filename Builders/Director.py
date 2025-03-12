8ca0c35b501bc112ceb6b4b2ad9fee496a923c39
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-03-12 19:14:26 -0300

    Atualizando Director

--- Builders/Director.py
+++ Builders/Director.py
@@ -3,23 +3,33 @@ from Builders.BuilderInfosContato import BuilderInfosContato
 from Builders.BuilderInfosPessoais import BuilderInfosPessoais
 from Builders.BuilderRedesSociais import BuilderRedesSociais
 from Builders.BuilderSessaoPrincipal import BuilderSessaoPrincipal
+from Model.ClassesAbstratas.ArmazenamentoGeralAbs import ArmazenamentoGeralAbs
+
 
 class Director:
-    def __init__(self):
-        pass
+    def __init__(self, armazenar: ArmazenamentoGeralAbs):
+        self.__armazenar = armazenar
 
     def aplicacao(self):
         dados_principais = BuilderDadosPrincipais()
         dados_principais.construir_sessao()
+        self.__armazenar.armazenar_dados_gerais(f'Dados Principais: {dados_principais.get_dados()}')
 
         infos_contato = BuilderInfosContato()
         infos_contato.construir_sessao()
+        self.__armazenar.armazenar_dados_gerais(f'Informações de Contato: {infos_contato.get_dados()}')
 
         infos_pessoais = BuilderInfosPessoais()
         infos_pessoais.construir_sessao()
+        self.__armazenar.armazenar_dados_gerais(f'Informações Pessoais: {infos_pessoais.get_dados()}')
 
         redes_sociais = BuilderRedesSociais()
         redes_sociais.construir_sessao()
+        self.__armazenar.armazenar_dados_gerais(f'Redes Sociais: {redes_sociais.get_dados()}')
 
         sessao_principal = BuilderSessaoPrincipal()
         sessao_principal.construir_sessao()
+        self.__armazenar.armazenar_dados_gerais(f'Sessão Principal: {sessao_principal.get_dados()}')
+
+    def get_dados(self):
+        return self.__armazenar.get_dados_gerais()
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from Builders.Director import Director
+import re
 
-app = Director()
-app.aplicacao()
+dados = ["Dados Principais: [[['sadasdasdaswwas'], ['asdasdasdas']]]", "Informações de Contato: [[['99999999999'], ['asdasdsad@asdasd.com'], ['sadawsmsx  asdasdsadasd']]]", "Informações Pessoais: [[['28/08/2000'], ['dedededed'], ['mncmncmxznczx']]]", "Redes Sociais: [[['sdasdasdasd', 'swswswswsws'], ['asdawsas', 'asasxsaxsaxsxa']]]", "Sessão Principal: [[[['asdawsawsa', '02/2000', '04/2024'], ['asdwsawswa', '01/2023', '04/2023']], ['aswwsawswaswsawssdasdasdasd', 'sadawswasadsadasdas'], [['asdasdsadasdasd', '04/2020', '05/2022'], ['asdwsawsaw', '03/2024', '03/2025'], ['asdawswwdscccc', '02/2022', '04/2024']], ['asdasa askxna xasnx aosx sjx  d lsjndc sdlck sdjc sdcsdcs']]]"]
+
+for dado in dados:
+    if "Informações de Contato:" in dado:
+        # Extrair os índices usando expressão regular
+        indices = re.findall(r'\[([^\]]+)\]', dado)
+        print(indices)
